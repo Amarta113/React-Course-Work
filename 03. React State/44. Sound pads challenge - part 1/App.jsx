71bc6a95d7bb447466f1152c15a69fb8e388d973
Amarta113 <amartawaghani@gmail.com>
2025-09-24 18:25:50 +0500

    Map over state array

--- 03. React State/44. Sound pads challenge - part 1/App.jsx
+++ 03. React State/44. Sound pads challenge - part 1/App.jsx
@@ -1,19 +1,23 @@
 import pads from "./pads"
+import React from "react"
+
 
 export default function App() {
-    /**
-     * Challenge part 1:
-     * 1. Initialize state with the default value of the
-     *    array pulled in from pads.js
-     * 2. Map over that state array and display each one
-     *    as a <button> (CSS is already written for you)
-     *    (Don't worry about using the "on" or "color" 
-     *    properties yet)
-     */
+    // Code : create states
+    const [values, setValues] = React.useState(pads)
+
+    // Mapping over states
+    const padItems = values.map(value => (
+        <button key={value.id}></button>
+    )
+    )
     return (
         <main>
             <div className="pad-container">
-                {/* <button>s go here */}
+                {
+                /* <button>s go here */
+                padItems
+                }
             </div>
         </main>
     )
---
+++ 03. React State/44. Sound pads challenge - part 1/Readme.md
@@ -0,0 +1,8 @@
+## Challenge part 1:
+- Initialize state with the default value of the
+array pulled in from pads.js
+- Map over that state array and display each one
+as a `<button>` (CSS is already written for you)
+(Don't worry about using the "on" or "color" properties yet)
+
+## Resource: 
