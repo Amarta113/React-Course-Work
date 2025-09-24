a05488432416b4000c0b2733db121ad911b7d25a
Amarta113 <amartawaghani@gmail.com>
2025-09-24 21:29:19 +0500

    Set darkMode background color condition with ternary operators

--- 03. React State/45. Dynamic styles/App.jsx
+++ 03. React State/45. Dynamic styles/App.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 import padsData from "./pads"
 
-export default function App() {
+export default function App(props) {
     const [pads, setPads] = React.useState(padsData)
     
-    /**
-     * Challenge: use a ternary to determine the backgroundColor
-     * of the buttons
-     * If darkMode is true, set them to "#222222"
-     * If darkMode is false, set them to "#cccccc"
-     */
+    // Set ternary operator to check the prop is true or false 
+    // and set background color as per true or false value
+    const styles = {
+        backgroundColor: props.darkMode? "#222222" : "#cccccc"
+    }
     
+    // Added style in button
     const buttonElements = pads.map(pad => (
-        <button key={pad.id}></button>
+        <button style={styles} key={pad.id}></button>
     ))
     
     return (
---
+++ 03. React State/45. Dynamic styles/Readme.md
@@ -0,0 +1,7 @@
+## Challenge: 
+Use a ternary to determine the backgroundColor of the buttons
+- If darkMode is true, set them to "#222222"
+- If darkMode is false, set them to "#cccccc"
+
+# Resource to learn ternary operator
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator
